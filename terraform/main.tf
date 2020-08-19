10987081e3a0f2694a0ed47fde0a703c75f476e5
Andy Hall <ajhall.us@gmail.com>
2020-08-18 20:57:41 -0400

    Point Heroku build source to new repo location

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,6 +40,6 @@ resource heroku_build build {
   app = heroku_app.app.id
 
   source = {
-    url     = "https://github.com/ajhall/terraform-heroku-facebook-gcal-sync/tarball/master"
+    url     = "https://github.com/ajhall/terraform-heroku-facebook-gcal-sync/archive/main.tar.gz"
   }
 }
